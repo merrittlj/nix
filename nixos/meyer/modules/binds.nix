126e9096037572750c39d08e10ea15be42433e03
Lucas Merritt <merrittlj@protonmail.com>
2024-09-23 20:38:10 -0600

    meyer: add brightness binds

--- nixos/meyer/modules/binds.nix
+++ nixos/meyer/modules/binds.nix
@@ -1,9 +1,14 @@
+{ pkgs, ... }:
 {
-  services.actkdb = {
+  services.actkbd = {
     enable = true;
 	bindings = [
-      { };
-	  { };
+      { keys = [ 225 ]; events = [ "key" ]; command = "${pkgs.brightness-control}/bin/brightness-control /sys/class/backlight/gmux_backlight/brightness up 25"; }
+      { keys = [ 224 ]; events = [ "key" ]; command = "${pkgs.brightness-control}/bin/brightness-control /sys/class/backlight/gmux_backlight/brightness down 25"; }
+      # found through: doas actkbd -n -s -d /dev/input/event7
+	  # up volume: 115
+	  # down volume: 114
+	  # mute: 113
 	];
   };
 }
