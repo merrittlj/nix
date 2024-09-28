7548c650ce207eb3f5737422dec9c6903e62e6d3
Lucas Merritt <merrittlj@protonmail.com>
2024-09-27 18:01:08 -0600

    finish meyer status bar, update rpbar package

--- nixos/meyer/modules/xsetroot.nix
+++ nixos/meyer/modules/xsetroot.nix
@@ -1,14 +1,34 @@
-{ pkgs, ... }: {
-  systemd.user.services.xsetroot = {
+{ pkgs, lib, ... }: {
+  systemd.services.xsetroot = {
     enable = true;
     description = "xsetroot every second";
     wantedBy = [ "graphical-session.target" ];
     after = [ "graphical-session.target" ];
     serviceConfig = {
       Type = "simple";
+      #Group = "wheel";
+      User = "lucas";
       Restart = "always";
-	  RestartSec = "5s";
-      ExecStart = "/bin/sh -c '${pkgs.xorg.xsetroot}/bin/xsetroot -name \"$(${pkgs.coreutils-full}/bin/date \"+%%m/%%d %%R\") $(${pkgs.battery}/bin/battery | ${pkgs.gnused}/bin/sed -E \"s/([0-9]*)\\..*/\\1/g\")%%\"'";
+	  RestartSec = "2s";
+
+      # xsetroot needs a display when running in root, we use -d :0
+      # iw dev spits out horizontal tabulations so we get rid of them with the "x09" sed
+      # battery sed changes xy.zw to xy%
+      ExecStart = lib.strings.concatStrings [
+        "+/bin/sh -c "
+        "'${pkgs.xorg.xsetroot}/bin/xsetroot -d :0 -name "
+        "\""
+        "$(${pkgs.iw}/bin/iw dev wlp4s0 link "
+          "| grep -i ssid "
+          "| ${pkgs.gnused}/bin/sed -E \"s/(\\s*SSID:\\s)(.*)/\\\\2/\" "
+          "| ${pkgs.gnused}/bin/sed -E \"s/\\x09//g\") "
+        "$(${pkgs.iw}/bin/iw dev wlp4s0 link "
+          "| grep -i signal "
+          "| ${pkgs.gnused}/bin/sed -E \"s/(\\s*signal:\\s)(.*)\\sdBm/\\\\2/\" "
+          "| ${pkgs.gnused}/bin/sed -E \"s/\\x09//g\") "
+        "$(${pkgs.coreutils-full}/bin/date \"+%%m/%%d %%R\") "
+        "$(${pkgs.battery}/bin/battery "
+          "| ${pkgs.gnused}/bin/sed -E \"s/([0-9]*)\\..*/\\\\1/g\")%%\"'"];
     };
   };
 }
