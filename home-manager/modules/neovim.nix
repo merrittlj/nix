c1b3f6c7d12a79bd664df14a605732d9ae465430
Lucas Merritt <merrittlj@protonmail.com>
2024-10-13 13:43:17 -0600

    add nixvim keymaps

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -1,4 +1,10 @@
 { pkgs, ... }:
+let
+  mkKeymap = mode: key: action: { inherit mode key action; };
+  mkKeymapWithOpts =
+    mode: key: action: opts:
+    (mkKeymap mode key action) // { options = opts; };
+in
 {
   programs.nixvim = {
     enable = true;
@@ -43,5 +49,19 @@
       onehalf
     ];
     colorscheme = "onehalflight";
+
+    keymaps = [
+      # Use jk/kj to escape insert mode
+      (mkKeymap "i" "jk" "<esc>")
+      (mkKeymap "i" "kj" "<esc>")
+      # Use C-l to remove search results highlight
+      (mkKeymapWithOpts "n" "<C-l>" ":nohls <CR> <C-l>" { silent = true; })
+      # Open files relative to the current one
+      (mkKeymap "n" ",e" ":e <C-R>=expand(\"%:p:h\") . \"/\" <CR>")
+      (mkKeymap "n" ",t" ":tabe <C-R>=expand(\"%:p:h\") . \"/\" <CR>")
+      (mkKeymap "n" ",s" ":split <C-R>=expand(\"%:p:h\") . \"/\" <CR>")
+      # Quote double quotes in current line
+      (mkKeymap "n" "qs" "0 :let hls = &hlsearch <CR> :set nohlsearch <CR> :.s#\"#\\\\\"#g <CR> :if hls | let @/ = '' | endif <CR> :let &hlsearch = hls <CR> 0")
+    ];
   };
 }
