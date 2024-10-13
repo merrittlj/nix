e7484f987777e7436ccba92c6d80bf7a06ab5832
Lucas Merritt <merrittlj@protonmail.com>
2024-10-13 00:28:11 -0600

    add nixvim configuration

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -1,5 +1,43 @@
 {
   programs.nixvim = {
     enable = true;
+    defaultEditor = true;
+	viAlias = true;
+	vimAlias = true;
+
+	opts = {
+	  path = ".,**";
+
+	  tabstop = 4;
+	  softtabstop = 4;
+	  shiftwidth = 4;
+	  expandtab = true;
+	  autoindent = true;
+
+	  relativenumber = true;
+	  display = "truncate";
+	  wrap = false;
+
+	  hlsearch = true;
+	  incsearch = true;
+	  showmatch = true;
+	  syntax = "on";
+
+	  ttimeout = true;
+	  ttimeoutlen = 1;
+	  ttyfast = true;
+
+      background = "light";
+	};
+
+	plugins = {
+      airline.enable = true;
+	  autoclose.enable = true;
+      nvim-surround.enable = true;
+	  fugitive.enable = true;
+	  commentary.enable = true;
+	};
+
+	colorschemes.one.enable = true;
   };
 }
