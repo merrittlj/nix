b78431ea15d0dbe5656878ceb8970c048e86f368
Lucas Merritt <merrittlj@protonmail.com>
2025-12-26 17:39:10 -0700

    laptop fixes

--- nixos/modules/sops.nix
+++ nixos/modules/sops.nix
@@ -27,18 +27,4 @@ in {
   };
 
   programs.ssh.startAgent = true;
-
-  systemd.user.services.ssh-add-keys = {
-    description = "Add SSH keys to agent";
-    wantedBy = [ "default.target" ];
-    after = [ "ssh-agent.service" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = pkgs.writeShellScript "ssh-add-keys" ''
-        ${pkgs.openssh}/bin/ssh-add ${config.sops.secrets."ssh_key/andesite_git".path}
-        ${pkgs.openssh}/bin/ssh-add ${config.sops.secrets."ssh_key/personal_git".path}
-      '';
-      Environment = "SSH_AUTH_SOCK=%t/ssh-agent.socket";
-    };
-  };
 }
