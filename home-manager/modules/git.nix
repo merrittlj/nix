bf8054da368d66d25e464c124654ecea07da4c13
Lucas Merritt <merrittlj@protonmail.com>
2026-02-06 10:50:40 -0700

    update git to not use default ssh

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -19,6 +19,8 @@
             commit.gpgSign = true;
             tag.gpgSign = true;
             gpg.format = "openpgp";
+
+            url."git@github.com-work:".insteadOf = "git@github.com:";
           };
         }
       ];
@@ -26,6 +28,5 @@
 
   programs.gh = {
     enable = false;
-	# gitCredentialHelper.enable = true;
   };
 }
--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -8,16 +8,22 @@ in
   programs.ssh = {
     enable = true;
 
+    controlMaster = "auto";
+    controlPath = "~/.ssh/master-%r@%n:%p";
+    controlPersist = "10m";  # Keep connections alive for 10 minutes after last use
+
     extraConfig = ''
-      Host github.com-personal
-        HostName github.com
+      Host github.com
+        HostName ssh.github.com
         User git
+        Port 443
         IdentityFile ${personalKey}
         IdentitiesOnly yes
 
       Host github.com-work
-        HostName github.com
+        HostName ssh.github.com
         User git
+        Port 443
         IdentityFile ${workKey}
         IdentitiesOnly yes
     '';
