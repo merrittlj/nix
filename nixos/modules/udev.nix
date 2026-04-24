ca4058e9a283203055720a39ab6b9caaaabf0d6f
Lucas Merritt <merrittlj@protonmail.com>
2026-04-24 02:19:35 -0600

    25.11

--- nixos/modules/udev.nix
+++ nixos/modules/udev.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
     services.udev = {
-        packages = [ pkgs.android-udev-rules pkgs.qmk-udev-rules ];
+        packages = [ pkgs.qmk-udev-rules ];
         extraRules = ''
             SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", MODE="0666"
         '';
